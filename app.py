a539848f7fe2b7b9d116c625d525cc29b3ded7d2
mariii02 <mechever18@eafit.edu.co>
2024-10-02 19:16:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Mi primera chamba, me acuerdo del día en el que de la chamba yo me enamoré")
+st.title("Mejores libros que he leído en el año")
 
-st.header ("Eladio carrion's story and why he is important to vallenato")
+st.header ("Este año he leído 24 libros, 6 de esos han sido perfectos.")
 
-st.write ("""Su carrera musical profesional comenzó en 2015. Sin embargo, tiempo antes ya era conocido como influencer en Puerto Rico haciendo parodias imitando las voces de artistas de la escena urbana. Posterior a esto, incursionó como compositor.2​ En 2017 colaboró en el sencillo «Me enamoré de una yal», junto a Ñengo Flow y Ele A el Dominio, siendo este su primer éxito como artista.3​ Sin embargo, la canción que lo llevó al reconocimiento internacional fue «Mi cubana» con Cazzu, Khea y Ecko en 2018.4​ En 2019 recibió su primer nominación en los Premios Juventud como Nueva generación urbana. Participó en la Velada del año 3 creada por Ibai Llanos
-En 2020 lanzó su álbum de estudio debut Sauce Boyz, el cual se posicionó en Top Latin Albums durante 10 semanas seguidas. Ese mismo año, recibió su primera nominación a los Premios Grammy Latinos por el sencillo «Kemba Walker» junto a Bad Bunny. En 2021, lanzó su segundo álbum Monarca, que estuvo nominado como mejor álbum de música urbana en los Grammy Latinos. Ese mismo año también colaboró en el sencillo «Eladio Carrión: BZRP Music Sessions, Vol. 40» junto a Bizarrap, que le valió una certificación de oro en España.""")
+st.write ("""Mi criterio para decidir qué libros han sido perfectos se basa meramente en mis gustos personales, como una lectora que disfruta de la temática fantástica y los ensayos de estudios feministas. Los libros que mencionaré no fueron necesariamente publicados este año, sino que corresponden a mi momento de lectura. Los recomiendo ampliamente.""")
 
 image = Image.open("EladioCarrion.jpg")
 st.image(image, caption = "Eladio el bromas Carrion")
